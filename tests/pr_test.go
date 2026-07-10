7c070e9c961afa079a41e0a5f8e27a0051df1981
Valerio Bontempi <valerio.bontempi@it.ibm.com>
2026-07-10 11:22:08 +0200

    fix: fixed test with fake password

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -22,29 +22,28 @@ const yamlLocation = "../common-dev-assets/common-go-assets/common-permanent-res
 
 var permanentResources map[string]interface{}
 
-func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptions {
+func TestMain(m *testing.M) {
 	var err error
 	permanentResources, err = common.LoadMapFromYaml(yamlLocation)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// getting apikey to use as registry password
-	ibmcloud_apiKey := os.Getenv("IBM_CLOUD_API_KEY")
+	log.Printf("Using scc_wp_secure_api_url %s", permanentResources["scc_wp_secure_api_url"])
+	log.Printf("Using scc_wp_instance_id %s", permanentResources["scc_wp_instance_id"])
+
+	os.Exit(m.Run())
+}
+
+func setupOptions(t *testing.T, prefix string, dir string, terraformVars map[string]interface{}) *testhelper.TestOptions {
 
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
 		Testing:       t,
 		TerraformDir:  dir,
 		Prefix:        prefix,
 		ResourceGroup: resourceGroup,
-		Region:        "us-south", // hardconfing region to use SCC WP instance from the same region
-		TerraformVars: map[string]interface{}{
-			"scc_wp_registry_scanner_registry_password":     ibmcloud_apiKey, // pragma: allowlist secret
-			"scc_wp_registry_scanner_secure_api_url":        permanentResources["scc_wp_secure_api_url"],
-			"scc_wp_instance_id":                            permanentResources["scc_wp_instance_id"],
-			"scc_wp_registry_scanner_registry_url":          "https://private.us-south.icr.io",   // hardconfing region to use SCC WP instance from the same region
-			"scc_wp_registry_scanner_registry_iam_endpoint": "https://private.iam.cloud.ibm.com", // hardconfing region to use SCC WP instance from the same region
-		},
+		Region:        "us-south", // hardcoding region to use SCC WP instance from the same region
+		TerraformVars: terraformVars,
 	})
 	return options
 }
@@ -53,7 +52,16 @@ func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptio
 func TestRunBasicExample(t *testing.T) {
 	t.Parallel()
 
-	options := setupOptions(t, "sccpwp-basic", basicExampleDir)
+	extTerraformVars := map[string]interface{}{
+		// setting fake password just for testing purposes
+		"scc_wp_registry_scanner_registry_password":     "1a2b3c4d5e6f7g8h9i0j", // pragma: allowlist secret
+		"scc_wp_registry_scanner_secure_api_url":        permanentResources["scc_wp_secure_api_url"],
+		"scc_wp_instance_id":                            permanentResources["scc_wp_instance_id"],
+		"scc_wp_registry_scanner_registry_url":          "https://private.us-south.icr.io",   // hardcoding region to use SCC WP instance from the same region
+		"scc_wp_registry_scanner_registry_iam_endpoint": "https://private.iam.cloud.ibm.com", // hardcoding region to use SCC WP instance from the same region
+	}
+
+	options := setupOptions(t, "sccpwp-basic", basicExampleDir, extTerraformVars)
 
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
@@ -64,7 +72,15 @@ func TestRunBasicExample(t *testing.T) {
 func TestRunUpgradeExample(t *testing.T) {
 	t.Parallel()
 
-	options := setupOptions(t, "sccpwp-basic-upg", basicExampleDir)
+	extTerraformVars := map[string]interface{}{
+		"scc_wp_registry_scanner_registry_password":     "1a2b3c4d5e6f7g8h9i0j", // pragma: allowlist secret
+		"scc_wp_registry_scanner_secure_api_url":        permanentResources["scc_wp_secure_api_url"],
+		"scc_wp_instance_id":                            permanentResources["scc_wp_instance_id"],
+		"scc_wp_registry_scanner_registry_url":          "https://private.us-south.icr.io",   // hardcoding region to use SCC WP instance from the same region
+		"scc_wp_registry_scanner_registry_iam_endpoint": "https://private.iam.cloud.ibm.com", // hardcoding region to use SCC WP instance from the same region
+	}
+
+	options := setupOptions(t, "sccpwp-basic-upg", basicExampleDir, extTerraformVars)
 
 	output, err := options.RunTestUpgrade()
 	if !options.UpgradeTestSkipped {
